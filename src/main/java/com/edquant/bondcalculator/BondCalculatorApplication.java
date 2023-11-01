1cdb96b61d1c39d291847fc2afc4dbd64e983862
Alexuc Eduard <alexuc.edy27@gmail.com>
2023-11-01 19:14:04 +0200

    first commit

---
+++ src/main/java/com/edquant/bondcalculator/BondCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.edquant.bondcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BondCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edquant/bondcalculator/BondCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edquant.bondcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
